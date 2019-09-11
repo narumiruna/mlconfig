ac849f18651918b9950a700ec5f83c5f01e7d4f7
なるみ <weaper@gmail.com>
2019-09-11 22:24:41 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+def main():
+    setup(
+        name='mlconfig',
+        version='0.1.0',
+        author='Narumi',
+        author_email='weaper@gamil.com',
+        packages=find_packages(),
+    )
+
+
+if __name__ == "__main__":
+    main()
