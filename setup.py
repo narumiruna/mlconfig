ddea489d9856b1042b2fe10be6858fc841135353
なるみ <weaper@gmail.com>
2019-09-12 03:19:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 def main():
     setup(
         name='mlconfig',
-        version='0.1.4',
+        version='0.1.5',
         author='Narumi',
         author_email='weaper@gamil.com',
         packages=find_packages(),
