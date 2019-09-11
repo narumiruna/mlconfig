f19298ebf3cc4373935a8dacc63920bdc7a0e8bd
なるみ <weaper@gmail.com>
2019-09-11 23:08:24 +0800

    Remove requirements.txt

--- setup.py
+++ setup.py
@@ -1,22 +1,14 @@
 from setuptools import find_packages, setup
 
 
-def parse_requirements(f):
-    lines = []
-    with open(f, 'r') as fp:
-        for line in fp.readlines():
-            lines.append(line.strip())
-    return lines
-
-
 def main():
     setup(
         name='mlconfig',
-        version='0.1.2',
+        version='0.1.4',
         author='Narumi',
         author_email='weaper@gamil.com',
         packages=find_packages(),
-        install_requires=parse_requirements('requirements.txt'),
+        install_requires=['pyyaml'],
     )
 
 
