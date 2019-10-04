bb6420415b9ee6af87bc5f9b1f334ea5e8c22bd4
なるみ <weaper@gmail.com>
2019-10-04 13:56:21 +0800

    Get version from git tag

--- setup.py
+++ setup.py
@@ -1,10 +1,22 @@
+import git
 from setuptools import find_packages, setup
+
+
+def get_committed_date(tag):
+    return tag.commit.committed_date
+
+
+def get_version():
+    tags = git.Repo().tags
+    tags = sorted(tags, key=get_committed_date, reverse=True)
+    version = str(tags[0]).lstrip('v')
+    return version
 
 
 def main():
     setup(
         name='mlconfig',
-        version='0.1.11',
+        version=get_version(),
         author='Narumi',
         author_email='weaper@gamil.com',
         packages=find_packages(),
