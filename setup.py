32712023e273e1897ab9367106c0c5f5f5740fb9
なるみ <weaper@gmail.com>
2019-10-04 14:48:05 +0800

    Get version from git tag (#7)

--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
+import subprocess
+
 from setuptools import find_packages, setup
+
+
+def get_version():
+    tag = subprocess.check_output(['git', 'describe', '--tags']).decode('utf-8')
+    version = tag.lstrip('v').split('-')[0]
+    return version
 
 
 def main():
     setup(
         name='mlconfig',
-        version='0.1.11',
+        version=get_version(),
         author='Narumi',
         author_email='weaper@gamil.com',
         packages=find_packages(),
