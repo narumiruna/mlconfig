b2c63d4e390548c331dbec6cd7a229f1eead36d3
なるみ <weaper@gmail.com>
2019-10-23 22:54:43 +0800

    Test utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -34,19 +34,22 @@ class TestUtils(unittest.TestCase):
         }
 
         self.yaml_path = os.path.join(tempfile.gettempdir(), 'test.yaml')
+        self.yml_path = os.path.join(tempfile.gettempdir(), 'test.yml')
         self.json_path = os.path.join(tempfile.gettempdir(), 'test.json')
 
     def test_save_dict(self):
         save_dict(self.data, self.yaml_path)
+        save_dict(self.data, self.yml_path)
         save_dict(self.data, self.json_path)
 
     def test_load_dict(self):
         yaml_data = load_dict(self.yaml_path)
+        yml_data = load_dict(self.yml_path)
         json_data = load_dict(self.json_path)
 
         self.assertDictEqual(self.data, yaml_data)
-        self.assertDictEqual(self.data, json_data)
-        self.assertDictEqual(yaml_data, json_data)
+        self.assertDictEqual(yaml_data, yml_data)
+        self.assertDictEqual(yml_data, json_data)
 
 
 def main():
