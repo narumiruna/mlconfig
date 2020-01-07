f8007dae9a66fe58c60a9968e1239b5247c5b53c
なるみ <weaper@gmail.com>
2020-01-07 14:55:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
-import subprocess
-
 from setuptools import find_packages, setup
-
-
-def get_version():
-    tag = subprocess.check_output(['git', 'describe', '--tags']).decode('utf-8')
-    version = tag.lstrip('v').split('-')[0]
-    return version
 
 
 def main():
     setup(
         name='mlconfig',
-        version=get_version(),
+        use_scm_version=True,
+        setup_requires=['setuptools_scm'],
         author='Narumi',
         author_email='weaper@gamil.com',
         packages=find_packages(),
