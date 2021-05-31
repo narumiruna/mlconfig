bb8b8f4086b3e4c1698c6a378da76e6e8c236abc
なるみ <weaper@gmail.com>
2021-05-31 17:24:06 +0800

    Remove instantiate

--- mlconfig/__init__.py
+++ mlconfig/__init__.py
@@ -1,4 +1,3 @@
-from .config import instantiate
 from .config import load
 from .config import register
 from .config import set_key_of_func_or_cls
--- tests/test_config.py
+++ tests/test_config.py
@@ -83,11 +83,3 @@ class TestConfig(unittest.TestCase):
 
         with self.assertRaises(ValueError):
             c1.merge(c2, allow_new_key=False)
-
-    def test_instantiate(self):
-        a = 1
-        b = 2
-        config = Config(name='AddOperator', a=a, b=b)
-
-        obj = mlconfig.instantiate(config)
-        self.assertEqual(obj.add(), a + b)
