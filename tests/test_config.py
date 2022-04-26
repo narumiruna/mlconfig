ef38bff78f270b89f93ac7df0318eb60243e6dbe
なるみ <narumi@maicoin.com>
2022-04-26 11:30:58 +0800

    migrate tests from unittest to pytest

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,7 @@
 import os
-import tempfile
-import unittest
+from tempfile import gettempdir
+
+import pytest
 
 import mlconfig
 from mlconfig.config import Config
@@ -17,84 +18,88 @@ class AddOperator(object):
         return self.a + self.b
 
 
-class TestConfig(unittest.TestCase):
-
-    def setUp(self):
-        self.data = {
-            'trainer': {
-                'name': 'ImageClassificationTrainer',
-                'num_epochs': 20
-            },
-            'dataset': {
-                'name': 'MNISTDataloader',
-                'root': 'data',
-                'batch_size': 256,
-                'num_workers': 0
-            },
-            'model': {
-                'name': 'LeNet'
-            },
-            'optimizer': {
-                'name': 'Adam',
-                'lr': 1e-3
-            },
-            'scheduler': {
-                'name': 'StepLR',
-                'step_size': 10,
-                'gamma': 0.1
-            }
+@pytest.fixture
+def raw_config():
+    return {
+        'trainer': {
+            'name': 'ImageClassificationTrainer',
+            'num_epochs': 20
+        },
+        'dataset': {
+            'name': 'MNISTDataloader',
+            'root': 'data',
+            'batch_size': 256,
+            'num_workers': 0
+        },
+        'model': {
+            'name': 'LeNet'
+        },
+        'optimizer': {
+            'name': 'Adam',
+            'lr': 1e-3
+        },
+        'scheduler': {
+            'name': 'StepLR',
+            'step_size': 10,
+            'gamma': 0.1
         }
-        self.temp_dir = tempfile.gettempdir()
+    }
 
-    def test_save_and_load_config(self):
-        c1 = Config(self.data)
-        f = os.path.join(self.temp_dir, 'test_save.yaml')
-        c1.save(f)
 
-        c2 = mlconfig.load(f)
-        self.assertDictEqual(c1.to_dict(), c2.to_dict())
+def test_config_save_and_load(raw_config):
+    c1 = Config(raw_config)
+    f = os.path.join(gettempdir(), 'test_save.yaml')
+    c1.save(f)
 
-    def test_instantiate(self):
-        a = 1
-        b = 2
-        config = Config(name='AddOperator', a=a, b=b)
+    c2 = mlconfig.load(f)
+    assert c1.to_dict() == c2.to_dict()
 
-        obj = config.instantiate()
-        self.assertEqual(obj.add(), a + b)
 
-    def test_call(self):
-        a = 1
-        b = 2
-        config = Config(name='AddOperator', a=a, b=b)
+def test_config_instantiate():
+    a = 1
+    b = 2
+    config = Config(name='AddOperator', a=a, b=b)
 
-        obj = config()
-        self.assertEqual(obj.add(), a + b)
+    obj = config.instantiate()
+    assert obj.add() == a + b
 
-    def test_merge(self):
-        c1 = Config(a=1, b=2)
-        c2 = Config(b=3, c=4)
 
-        c1.merge(c2, allow_new_key=True)
-        self.assertDictEqual(c1.to_dict(), dict(a=1, b=3, c=4))
+def test_config_call():
+    a = 1
+    b = 2
+    config = Config(name='AddOperator', a=a, b=b)
 
-    def test_merge_allow_new_key(self):
-        c1 = Config(a=1, b=2)
-        c2 = Config(b=3, c=4)
+    obj = config()
+    assert obj.add() == a + b
 
-        with self.assertRaises(ValueError):
-            c1.merge(c2, allow_new_key=False)
 
-    def test_variable_expansion(self):
-        data = self.data.copy()
-        data['num_classes'] = 50
-        data['model']['num_classes'] = '$num_classes'
+def test_config_merge():
+    c1 = Config(a=1, b=2)
+    c2 = Config(b=3, c=4)
 
-        data['prefix'] = '/usr/share'
-        data['dataset']['root'] = '$prefix/data'
+    c1.merge(c2, allow_new_key=True)
+    assert c1.to_dict() == dict(a=1, b=3, c=4)
 
-        data['output'] = '$prefix/${model.name}'
 
-        config = mlconfig.load(data)
-        self.assertEqual(config['model']['num_classes'], 50)
-        self.assertEqual(config['dataset']['root'], '/usr/share/data')
-        self.assertEqual(config['output'], '/usr/share/LeNet')
+def test_config_merge_allow_new_key():
+    c1 = Config(a=1, b=2)
+    c2 = Config(b=3, c=4)
+
+    with pytest.raises(ValueError):
+        c1.merge(c2, allow_new_key=False)
+
+
+def test_variable_expansion(raw_config):
+    data = raw_config.copy()
+    data['num_classes'] = 50
+    data['model']['num_classes'] = '$num_classes'
+
+    data['prefix'] = '/usr/share'
+    data['dataset']['root'] = '$prefix/data'
+
+    data['output'] = '$prefix/${model.name}'
+
+    config = mlconfig.load(data)
+    assert config['model']['num_classes'] == 50
+    assert config['dataset']['root'] == '/usr/share/data'
+    assert config['output'] == '/usr/share/LeNet'
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,87 +1,92 @@
 import os
-import tempfile
-import unittest
+from tempfile import gettempdir
 
-from mlconfig.utils import isextension, load_dict, save_dict
+import pytest
+
+from mlconfig.utils import isextension
+from mlconfig.utils import load_dict
+from mlconfig.utils import save_dict
 
 
-class TestUtils(unittest.TestCase):
-
-    def setUp(self):
-        self.data = {
-            'trainer': {
-                'name': 'ImageClassificationTrainer',
-                'num_epochs': 20
-            },
-            'dataset': {
-                'name': 'MNISTDataloader',
-                'root': 'data',
-                'batch_size': 256,
-                'num_workers': 0
-            },
-            'model': {
-                'name': 'LeNet'
-            },
-            'optimizer': {
-                'name': 'Adam',
-                'lr': 1e-3
-            },
-            'scheduler': {
-                'name': 'StepLR',
-                'step_size': 10,
-                'gamma': 0.1
-            }
+@pytest.fixture
+def raw_config():
+    return {
+        'trainer': {
+            'name': 'ImageClassificationTrainer',
+            'num_epochs': 20
+        },
+        'dataset': {
+            'name': 'MNISTDataloader',
+            'root': 'data',
+            'batch_size': 256,
+            'num_workers': 0
+        },
+        'model': {
+            'name': 'LeNet'
+        },
+        'optimizer': {
+            'name': 'Adam',
+            'lr': 1e-3
+        },
+        'scheduler': {
+            'name': 'StepLR',
+            'step_size': 10,
+            'gamma': 0.1
         }
+    }
 
-        self.temp_dir = tempfile.gettempdir()
-        self.yaml_path = os.path.join(self.temp_dir, 'test.yaml')
-        self.yml_path = os.path.join(self.temp_dir, 'test.yml')
-        self.json_path = os.path.join(self.temp_dir, 'test.json')
 
-    def test_save_and_load_dict(self):
-        save_dict(self.data, self.yaml_path)
-        save_dict(self.data, self.yml_path)
-        save_dict(self.data, self.json_path)
+def test_utils_save_and_load_dict(raw_config):
+    yaml_path = os.path.join(gettempdir(), 'test.yaml')
+    yml_path = os.path.join(gettempdir(), 'test.yml')
+    json_path = os.path.join(gettempdir(), 'test.json')
 
-        yaml_data = load_dict(self.yaml_path)
-        yml_data = load_dict(self.yml_path)
-        json_data = load_dict(self.json_path)
+    save_dict(raw_config, yaml_path)
+    save_dict(raw_config, yml_path)
+    save_dict(raw_config, json_path)
 
-        self.assertDictEqual(self.data, yaml_data)
-        self.assertDictEqual(yaml_data, yml_data)
-        self.assertDictEqual(yml_data, json_data)
+    yaml_data = load_dict(yaml_path)
+    yml_data = load_dict(yml_path)
+    json_data = load_dict(json_path)
 
-    def test_load_dict_value_error(self):
-        with self.assertRaises(ValueError):
-            load_dict(os.path.join(self.temp_dir, 'test'))
+    assert raw_config == yaml_data
+    assert yaml_data == yml_data
+    assert yml_data == json_data
 
-        with self.assertRaises(ValueError):
-            load_dict(os.path.join(self.temp_dir, 'test.py'))
 
-    def test_save_dict_value_error(self):
-        with self.assertRaises(ValueError):
-            save_dict(self.data, os.path.join(self.temp_dir, 'test'))
+def test_utils_load_dict_value_error():
+    with pytest.raises(ValueError):
+        load_dict(os.path.join(gettempdir(), 'test'))
 
-        with self.assertRaises(ValueError):
-            save_dict(self.data, os.path.join(self.temp_dir, 'test.py'))
+    with pytest.raises(ValueError):
+        load_dict(os.path.join(gettempdir(), 'test.py'))
 
-    def test_isextension(self):
-        self.assertTrue(isextension('test.jpg', '.jpg'))
-        self.assertTrue(isextension('test.jpg', ('.jpg',)))
-        self.assertTrue(isextension('test.jpg', ('.jpg', '.png')))
 
-        self.assertTrue(isextension('.test.jpg', '.jpg'))
-        self.assertTrue(isextension('.test.jpg', ('.jpg',)))
-        self.assertTrue(isextension('.test.jpg', ('.jpg', '.png')))
+def test_utils_save_dict_value_error(raw_config):
+    with pytest.raises(ValueError):
+        save_dict(raw_config, os.path.join(gettempdir(), 'test'))
 
-        self.assertFalse(isextension('test', '.jpg'))
-        self.assertFalse(isextension('test', ('.jpg',)))
-        self.assertFalse(isextension('test', ('.jpg', '.png')))
+    with pytest.raises(ValueError):
+        save_dict(raw_config, os.path.join(gettempdir(), 'test.py'))
 
-        self.assertFalse(isextension('', '.jpg'))
-        self.assertFalse(isextension('', ('.jpg',)))
-        self.assertFalse(isextension('', ('.jpg', '.png')))
 
-        self.assertFalse(isextension('.', '.jpg'))
-        self.assertFalse(isextension('.', ('.jpg',)))
-        self.assertFalse(isextension('.', ('.jpg', '.png')))
+def test_utils_isextension():
+    assert isextension('test.jpg', '.jpg')
+    assert isextension('test.jpg', ('.jpg',))
+    assert isextension('test.jpg', ('.jpg', '.png'))
+
+    assert isextension('.test.jpg', '.jpg')
+    assert isextension('.test.jpg', ('.jpg',))
+    assert isextension('.test.jpg', ('.jpg', '.png'))
+
+    assert not isextension('test', '.jpg')
+    assert not isextension('test', ('.jpg',))
+    assert not isextension('test', ('.jpg', '.png'))
+
+    assert not isextension('', '.jpg')
+    assert not isextension('', ('.jpg',))
+    assert not isextension('', ('.jpg', '.png'))
+
+    assert not isextension('.', '.jpg')
+    assert not isextension('.', ('.jpg',))
+    assert not isextension('.', ('.jpg', '.png'))
